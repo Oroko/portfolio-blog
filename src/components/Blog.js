61d2b3f9c913d3b73318d104e75e36395cc0caff
Oroko <aoroko09@gmail.com>
2021-08-11 00:50:35 -0400

    Deleted a couple of test files and added a couple of folders containers, actions and reducer folders and created some index.js files in them

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import testAction from '../actions/test'
+import {useSelector} from 'react-redux'
 
 function Blog() {
+  const test = useSelector(state => state.testReducer)
   return (
     <div>
-      Blogs
+      {test}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from "./reducers/index";
 
 
-import App from "./App";
+import App from "./App"; 
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-
   <React.StrictMode>
-  
+    <Provider store={store}>
       <ChakraProvider>
-        <ColorModeScript initialColorMode='Dark'></ColorModeScript>
+        <ColorModeScript initialColorMode="Dark"></ColorModeScript>
         <App />
       </ChakraProvider>
-    
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
